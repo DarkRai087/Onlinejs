c0d28af002c25dc8ea76c6dc78cd5d910400d1c7
Parth Pandey <parthp90111@gmail.com>
2024-07-27 12:30:56 +0530

    =>

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-
-async function runCode() {
+const runCode = async () => {
     const code = document.getElementById('code-area').value;
     const output = document.getElementById('output');
     output.textContent = '';
 
     try {
         const oldLog = console.log;
-        console.log = function(...args) {
+        console.log = (...args) => {
             output.textContent += args.join(' ') + '\n';
             oldLog.apply(console, args);
         };
@@ -18,34 +17,34 @@ async function runCode() {
     } catch (error) {
         output.textContent = 'Error: ' + error.message;
     }
-}
+};
 
-function redirectToLink() {
-    window.location.href = 'https://github.com/DarkRai087/Onlinejs'; 
-}
+const redirectToLink = () => {
+    window.location.href = 'https://github.com/DarkRai087/Onlinejs';
+};
 
-function clearCode() {
+const clearCode = () => {
     document.getElementById('code-area').value = '';
     document.getElementById('output').textContent = '';
-}
+};
 
-function copyCode() {
+const copyCode = () => {
     const codeArea = document.getElementById('code-area');
     codeArea.select();
-    codeArea.setSelectionRange(0, 99999); 
+    codeArea.setSelectionRange(0, 99999);
     document.execCommand('copy');
-}
+};
 
-function impCode() {
+const impCode = () => {
     const fileInput = document.createElement('input');
     fileInput.type = 'file';
     fileInput.accept = '.txt,.js';
 
-    fileInput.addEventListener('change', function(event) {
+    fileInput.addEventListener('change', (event) => {
         const file = event.target.files[0];
         if (file) {
             const reader = new FileReader();
-            reader.onload = function(e) {
+            reader.onload = (e) => {
                 const contents = e.target.result;
                 document.getElementById('code-area').value = contents;
             };
@@ -54,13 +53,13 @@ function impCode() {
     });
 
     fileInput.click();
-}
+};
 
-function formatCode() {
+const formatCode = () => {
     const codeArea = document.getElementById('code-area');
     const formattedCode = prettier.format(codeArea.value, {
         parser: "babel",
         plugins: prettierPlugins,
     });
     codeArea.value = formattedCode;
-}
+};
